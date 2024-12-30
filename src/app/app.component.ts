80b40374ee69fb21ca0b3526f42771d9e13c93ee
Garret Premo <garretpremo@gmail.com>
2024-12-30 11:10:19 -0500

    introduce set-utilities library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { union } from 'set-utilities';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-19-test';
+
+  a = new Set([1,2,3]);
+  b = new Set([4,5,6]);
+  c = union(this.a, this.b);
 }
